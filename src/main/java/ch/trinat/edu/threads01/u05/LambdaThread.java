aac9a73d14fc4d7f95f4221eda0f4148080ccce1
Jules <jules67310r@gmail.com>
2021-01-05 14:49:34 +0100

    threads

---
+++ src/main/java/ch/trinat/edu/threads01/u05/LambdaThread.java
@@ -0,0 +1,15 @@
+package ch.trinat.edu.threads01.u05;
+
+public class LambdaThread {
+
+    public static void main(String[] args) {
+
+        new Thread(() -> {
+            System.out.println("Lambda");
+            for (int i = 0; i < 50; i++) {
+                String name = Thread.currentThread().getName();
+                System.out.println("Thread " + name + " hat Nummer " + i + " verarbeitet");
+            }
+        }, "Lambda").start();
+    }
+}
---
+++ src/main/java/ch/trinat/edu/threads01/u07/Worker.java
@@ -0,0 +1,19 @@
+package ch.trinat.edu.threads01.u07;
+
+public class Worker extends Thread {
+    Counter counter;
+    Worker(Counter counter) {
+        this.counter = counter;
+    }
+    public void run() {
+        synchronized(this) {
+            System.out.println("Thread " + this.getName() + " started!");
+            while (true) {
+                for (int i = 0; i <= Counter.LIMIT; i++)
+                    counter.up();
+                for (int i = 0; i < Counter.LIMIT; i++)
+                    counter.down();
+            }
+        }
+    }
+}
